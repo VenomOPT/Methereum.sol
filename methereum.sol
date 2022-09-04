59cff04910872a99d06f5c558cb4581519c8d549
Marin Constantin Horobet <111706680+VenomOPT@users.noreply.github.com>
2022-09-04 14:09:30 +0200

    Create methereum.sol

---
+++ methereum.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+
+/// @custom:security-contact mhorobet7@gmail.com
+contract Methereum is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit, ERC20FlashMint {
+    constructor() ERC20("Methereum", "MTH") ERC20Permit("Methereum") {
+        _mint(msg.sender, 1005672607374 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
